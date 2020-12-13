a2ccde057850e9313165a47f0856b560b41a4655
Stas_Krasnagir <stas220693@gmail.com>
2020-12-13 14:21:27 +0300

    13-12-2020

--- word_search.py
+++ word_search.py
@@ -1,2 +1,21 @@
+import unittest
+
+
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
+    def exist_word(self, board: List[List[str]], word: str) -> bool:
+        pass
+
+
+board = [["A", "B", "C", "E"],
+         ["S", "F", "C", "S"],
+         ["A", "D", "E", "E"]]
+word = "ABCCED"
+
+
+class TestSolution(unittest.TestCase):
+    def test_exist(self):
+        self.assertEqual((self.exist_word(board, word)), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
